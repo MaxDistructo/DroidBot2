4d69602f04fc51486e84a7d66efa853480cfe2f0
MaxDistructo <MaxDistructo@users.noreply.github.com>
2017-10-28 13:16:48 -0500

    Update Listener.java

--- src/main/java/maxdistructo/droidbot2/background/Listener.java
+++ src/main/java/maxdistructo/droidbot2/background/Listener.java
@@ -30,6 +30,8 @@ import org.json.JSONObject;
 import sx.blah.discord.util.RoleBuilder;
 
 import static maxdistructo.droidbot2.BaseBot.client;
+import static maxdistructo.droidbot2.BaseBot.playerFun;
+import static maxdistructo.droidbot2.BaseBot.pfListener;
 
 public class Listener {
     public static boolean blackJackRunning = false;
@@ -75,6 +77,27 @@ public class Listener {
             }
             catch(JSONException e){
                 e.printStackTrace();
+            }
+            if(playerFun){
+                LOGGER.info("PlayerFun Commands Enabled");
+                client.registerListener(pfListener);
+            }
+            else{
+                LOGGER.warn("PlayerFun Commands Not Enabled");
+            }
+            if(casino){
+                LOGGER.info("Casino Commands Enabled");
+                client.registerListener(casinoListener);
+            }
+            else{
+                LOGGER.warn("Casino Commands Not Enabled");
+            }
+            if(admin){
+                LOGGER.info("Admin Commands Enabled");
+                client.registerListener(adminListener);
+            }
+            else{
+                LOGGER.warn("Admin Commands Not Enabled");
             }
 
             if (!Roles.checkForBotAbuse(message)) {
@@ -126,50 +149,6 @@ public class Listener {
                     message.delete();
                 } else if (messageContent[0].equals(prefix + "allin") && Perms.checkGames(message)) {
                     message.reply("", Message.simpleEmbed(message.getAuthor(), "Allin", Allin.onAllinCommand(messageContent, message), message));
-                    message.delete();
-                } else if (messageContent[0].equals(prefix + "say") && channelMention != null) {
-                    Message.sendMessage(channelMention, Say.onSayCommand(messageContent, message, channelMention));
-                } else if (messageContent[0].equals(prefix + "say")) {
-                    Message.sendMessage(message.getChannel(), Say.onSayCommand(messageContent, message, channelMention));
-                } else if (messageContent[0].equals(prefix + "spam")) {
-                    Message.sendMessage(message.getChannel(), Spam.onSpamCommand(messageContent, message, mentioned));
-                    message.delete();
-                } else if (messageContent[0].equals(prefix + "slap")) {
-                    Message.sendMessage(message.getChannel(), PlayerFun.onSlapCommand(message, mentioned));
-                    message.delete();
-                } else if (messageContent[0].equals(prefix + "tnt")) {
-                    Message.sendMessage(message.getChannel(), PlayerFun.onTntCommand(message, mentioned));
-                    message.delete();
-                } else if (messageContent[0].equals(prefix + "kiss")) {
-                    Message.sendMessage(message.getChannel(), PlayerFun.onKissCommand(message, mentioned));
-                    message.delete();
-                } else if (messageContent[0].equals(prefix + "hug")) {
-                    Message.sendMessage(message.getChannel(), PlayerFun.onHugCommand(message, mentioned));
-                    message.delete();
-                } else if (messageContent[0].equals(prefix + "poke")) {
-                    Message.sendMessage(message.getChannel(), PlayerFun.onPokeCommand(message, mentioned));
-                    message.delete();
-                } else if (messageContent[0].equals(prefix + "respect") || messageContent[0].equals("/f")) {
-                    Message.sendMessage(message.getChannel(), PlayerFun.onPayRespects(message, mentioned));
-                    message.delete();
-                } else if (messageContent[0].equals(prefix + "banhammer")) {
-                    Message.sendMessage(message.getChannel(), PlayerFun.onBanHammer(message, mentioned));
-                    message.delete();
-                } else if (messageContent[0].equals(prefix + "shoot")) {
-                    Message.sendMessage(message.getChannel(), PlayerFun.onShootCommand(message, mentioned));
-                    message.delete();
-                } else if (messageContent[0].equals(prefix + "stab")) {
-                    Message.sendMessage(message.getChannel(), PlayerFun.onStabCommand(message, mentioned));
-                    message.delete();
-                } //else if (messageContent[0].equals(prefix + "mute")) {
-                   // Message.sendMessage(message.getChannel(), PlayerFun.onMuteCommand(message, mentioned));
-                   // message.delete();
-                    // } else if (messageContent[0].equals(prefix + "lenny") || messageContent[0].equals("/lenny")) {
-                    //     message.edit(PlayerFun.onLennyCommand());
-                    // } else if (messageContent[0].equals("/shrug")) { Won't work cause F U Discord.
-                    //      message.edit(PlayerFun.onShrugCommand());
-                 else if (messageContent[0].equals(prefix + "xp")) {
-                    Message.sendMessage(message.getChannel(), PlayerFun.onXpCommand(mentioned));
                     message.delete();
                 } else if (messageContent[0].equals(prefix + "punch")) {
                     Message.sendMessage(message.getChannel(), PlayerFun.onPunchCommand(message, mentioned));
